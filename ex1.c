a77b572e1bbef2f386bb880af9c230a13ccd5a9b
Gurnoor Kaur <85249027+kgurnoor@users.noreply.github.com>
2024-05-22 16:17:17 +0530

    Assignment 1
    This program prints a table of the number you enter.

---
+++ ex1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number you want multiplication table of: \n");
+    scanf("%d",&num);
+    printf("Table of %d: \n", num);
+    printf("%d * 1 = %d\n",num, num*1);
+    printf("%d * 1 = %d\n",num, num*2);
+    printf("%d * 1 = %d\n",num, num*3);
+    printf("%d * 1 = %d\n",num, num*4);
+    printf("%d * 1 = %d\n",num, num*5);
+    printf("%d * 1 = %d\n",num, num*6);
+    printf("%d * 1 = %d\n",num, num*7);
+    printf("%d * 1 = %d\n",num, num*8);
+    printf("%d * 1 = %d\n",num, num*9);
+    printf("%d * 1 = %d\n",num, num*10);
+    return 0;
+}
