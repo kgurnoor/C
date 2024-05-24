de30cf7f268a72635a9442d0f053be15f1749f96
Gurnoor Kaur <85249027+kgurnoor@users.noreply.github.com>
2024-05-24 11:48:21 +0530

    functions in C
    Program to learn different functions, with/without arguments and with/without a return value.

---
+++ functions.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int sum(int a, int b); //with argument with return value
+void printstar(int n); //with argument without return
+int takenumber(); //without argument with return
+
+int main()
+{
+    int a , b , c;
+    a =  4;
+    b = 6;
+    sum(a,b);
+    printstar(7);
+    takenumber();
+    return 0;
+}
+
+//Function 1
+int sum(int a, int b)
+{   
+    int c;
+    c = a + b;
+    printf("The sum of a and b is %d: ", c );
+    return 0;
+}
+
+//Function 2
+void printstar(int n)
+{
+    for (int i = 0;i < n;i++)
+    {
+        printf("%c", '*');
+    }    
+}
+
+//Function 3
+int takenumber()
+{
+    int p;
+    printf("Enter a number:\n");
+    scanf("%d",&p);
+    printf("The number you entered is: %d \n",p);
+    return 0;
+}
