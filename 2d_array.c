4b33b622c524ec86c10c4a737503013e18e98640
Gurnoor Kaur <85249027+kgurnoor@users.noreply.github.com>
2024-05-29 15:43:31 +0530

    two dimensional array with rows and columns

---
+++ 2d_array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int marks[2][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}};
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("The value of %d, %d element of the array is %d\n", i, j, marks[i][j]);
+        }
+    }
+}
